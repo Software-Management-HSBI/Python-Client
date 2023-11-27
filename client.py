a3cead03b42a51817cf1a72fe08013f1128f88a0
PDeimel <philipp.deimel@fh-bielefeld.de>
2023-11-27 15:37:01 +0100

    Erste Assets und Beginn der Client-Datei

---
+++ client.py
@@ -0,0 +1,22 @@
+import socketio
+
+
+class SocketIOClient:
+    def __init__(self):
+        self.server_address = ""
+
+        self.sio = socketio.Client(logger=True)
+
+        self.sio.on('connection_success', self.connection_succes)
+    
+    def connection_succes(self):
+        print("Connection established")
+
+
+    def connect(self):
+        try:
+            self.sio.connect()
+        except ConnectionError:
+            print("Couldn't connect")
+
+
