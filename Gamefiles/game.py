89e4ca9bbacc397ecad76e7a89dba72862e4ddb6
PDeimel <philipp.deimel@fh-bielefeld.de>
2023-12-07 16:50:31 +0100

    render() jetzt in separater Klasse

--- Gamefiles/game.py
+++ Gamefiles/game.py
@@ -8,6 +8,7 @@ from Visuals.player import Player
 from Visuals.background import Background
 from Visuals.road import Road
 from Visuals.colors import Colors
+from Visuals.render import Render
 
 import globals as gl
 
@@ -74,7 +75,7 @@ class Game:
                     if event.key == pygame.K_DOWN:
                         gl.keySlower = False
 
-            self.render()
+            Render.render()
             self.update(gl.STEP)
             pygame.display.update()
 
@@ -243,71 +244,6 @@ class Game:
          else:
               return Colors.get_dark()
 
-
-    
-    # Setzt ab einem bestimmten Punkt die Distanz der Strecke zurueck (?) und zeichnet Strecke, Nebel und Spieler auf den Bildschirm
-    def render(self):
-        base = Util.which_segment(gl.position)
-        base_percent = Util.percent_remaining(gl.position, gl.segmentLength)
-        current_segment = Util.which_segment(gl.position + gl.playerZ)
-        current_percent = Util.percent_remaining(gl.position + gl.playerZ, gl.segmentLength)
-        playerY = Util.interpolate(current_segment.get("p1").get("world").get("y"), current_segment.get("p2").get("world").get("y"), current_percent)
-
-        dx = -(base.get("curve") * base_percent)
-        x = 0
-        maxY = gl.height
-
-        gl.background_sprites.draw(gl.screen)
-
-        for n in range(gl.drawDistance):
-            segment = gl.segments[(base.get("index") + n) % len(gl.segments)]
-            segment_looped = segment.get("index") < base.get("index")
-            segment_fog = Util.exponential_fog(n / gl.drawDistance, gl.fogDensity)
-            segment["clip"] = maxY
-
-            if segment_looped:
-                segment_looped_value = gl.trackLength
-            else:
-                segment_looped_value = 0
-
-            segment["p1"] = Util.project(
-                segment.get("p1"),
-                (gl.playerX * gl.roadWidth) - x,
-                playerY + gl.cameraHeight,
-                gl.position - segment_looped_value,
-                gl.cameraDepth,
-                gl.width, gl.height,
-                gl.roadWidth)
-
-            segment["p2"] = Util.project(
-                segment.get("p2"),
-                (gl.playerX * gl.roadWidth) -x - dx,
-                playerY + gl.cameraHeight,
-                gl.position - segment_looped_value,
-                gl.cameraDepth,
-                gl.width, gl.height,
-                gl.roadWidth)
-            
-            x += dx
-            dx += segment.get("curve")
-
-            if (segment.get("p1").get("camera").get("z") <= gl.cameraDepth) or (
-                    segment.get("p2").get("screen").get("y") >= maxY) or (
-                    segment.get("p2").get("screen").get("y") >= segment.get("p1").get("screen").get("y")):
-                continue
-
-            Util.segment(gl.screen, gl.width, gl.lanes,
-                        segment.get("p1").get("screen").get("x"),
-                        segment.get("p1").get("screen").get("y"),
-                        segment.get("p1").get("screen").get("w"),
-                        segment.get("p2").get("screen").get("x"),
-                        segment.get("p2").get("screen").get("y"),
-                        segment.get("p2").get("screen").get("w"),
-                        segment.get("color"), segment_fog)
-
-            maxY = segment.get("p1").get("screen").get("y")
-
-            gl.player_sprites.draw(gl.screen)   
 
     # Erstellt mit einer Hilfsklasse die einzelnen Hintergrundschichten
     def create_background(self):
