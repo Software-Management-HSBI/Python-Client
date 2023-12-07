51b0d2e551703ffd42513459c581ca9979bad1e3
PDeimel <philipp.deimel@fh-bielefeld.de>
2023-12-07 17:02:50 +0100

    Straßen-Erstellung jetzt separat

--- Gamefiles/game.py
+++ Gamefiles/game.py
@@ -6,28 +6,11 @@ import time
 from Gamefiles.util import Util
 from Visuals.player import Player
 from Visuals.background import Background
-from Visuals.road import Road
 from Visuals.colors import Colors
 from Visuals.render import Render
+from Visuals.roadCreation import Road
 
 import globals as gl
-
-# Hiermit bildet man die Strasse. Es wird wahrscheinlich wichtig sein, dass dieses Array genauso auch beim Java-Client aufgebaut ist.
-# 1. Variable ist Strassenlaenge, die kann man so einstellen wie man will, passt
-# 2. Variable gibt an, wie scharf eine Kurve ist, alle Werte davon muessen am Ende 0 ergeben
-# 3. Variable gibt an, wie steil ein Huegel ist, alle Werte davon muessen am Ende 0 ergeben
-# TODO: Gemeinsame Speicherloesung finden
-road = [
-    [100, 0, 60],
-    [50],
-    [100, 0, -60],
-    [90, 4],
-    [50],
-    [90, -4],
-    [75, 3, 40],
-    [25],
-    [100, -3, -40]
-]
 
 # Der Teil des Singleplayers, der alle anderen aktiviert: Hier werden Tasteneingaben überprüft und die Straße unendlich erweitert
 class Game:
@@ -42,7 +25,7 @@ class Game:
         gl.player_sprites = pygame.sprite.Group()
         gl.background_sprites = pygame.sprite.Group()
         gl.lap_start_time = time.time()
-        self.reset_road()
+        Road.reset_road()
         self.game_loop()
 
     # Loop des Spiels: Erstellt Spieler und Hintergrund, nimmt Inputs entgegen und ruft permanent die render- und update-Methode auf
@@ -146,103 +129,6 @@ class Game:
         gl.screen.blit(best_time_text, (10, 90))
         
     # Liest das Strassen-Array aus und markiert Start-/Ziellinie
-    def reset_road(self):
-
-        self.read_road()
-
-        gl.segments[Util.which_segment(gl.playerZ)["index"] + 2]["color"] = Colors.get_start()
-        gl.segments[Util.which_segment(gl.playerZ)["index"] + 3]["color"] = Colors.get_start()
-        for n in range(gl.rumbleLength):
-            gl.segments[len(gl.segments)-1-n]["color"] = Colors.get_finish()
-
-        gl.trackLength = len(gl.segments) * gl.segmentLength
-    
-    # Hier wird das hinzuzufuegende Segment anhand von Kurven- und Huegel-Parametern angepasst
-    def add_segment(self, curve, y):
-        n = len(gl.segments)
-        # Diese Werte muessen (hoffentlich) nicht mehr geaendert werden, jetzt wo Kurven (curve-Parameter) und Huegel (y-Parameter) benutzt werden
-        gl.segments.append(
-                {
-                    'index': n,
-                    'p1':
-                        {'world': {
-                            'x': None,
-                            'y': self.lastY(),
-                            'z': n * gl.segmentLength
-                        },
-                            'camera': {
-                                'x': 0,
-                                'y': 0,
-                                'z': 0
-                            },
-                            'screen': {
-                                "scale": 0,
-                                'x': 0,
-                                'y': 0,
-                            },
-                        },
-                    'p2':
-                        {'world': {
-                            'x': None,
-                            'y': y,
-                            'z': (n + 1) * gl.segmentLength
-                        },
-                            'camera': {
-                                'x': 0,
-                                'y': 0,
-                                'z': 0
-                            },
-                            'screen': {
-                                "scale": 0,
-                                'x': 0,
-                                'y': 0,
-                            },
-                        },
-                        "curve": curve,
-                        "cars": [],
-                        "clip": 0,
-                    "color": self.which_road(n)
-                }
-            )
-
-    # Fuegt Strassenteile an das Segment-Array hinzu
-    def add_road(self, enter, hold, leave, curve, y=0):
-        startY = self.lastY()
-        endY = startY + (int(y)*gl.segmentLength)
-        total = int(enter) + int(hold) + int(leave)
-        
-        for n in range(int(enter)):
-            self.add_segment(Util.easeIn(0, curve, n / enter), Util.easeInOut(startY, endY, n / total))
-        
-        for n in range(int(hold)):
-            self.add_segment(curve, Util.easeInOut(startY, endY, (enter + n) / total))
-
-        for n in range(int(leave)):
-            self.add_segment(Util.easeInOut(0, curve, n / enter), Util.easeInOut(startY, endY, (enter + hold + n) / total))
-
-    # Ueberprueft, ob Werte des Straßenmoduls leer ist
-    def add_street(self, num=None, curve=None, height=None):
-        if num is None:
-            num = Road.length().get("medium")
-        if curve is None:
-            curve = Road.curve().get("none")
-        if height is None:
-            height = Road.hill().get("none")
-
-        self.add_road(num, num, num, curve, height)
-
-    # Gibt die letzte Y-Koordinate aus, um einen glatten Huegel zu modellieren
-    def lastY(self):
-        if len(gl.segments) == 0:
-            return 0
-        return gl.segments[len(gl.segments)-1].get("p2").get("world").get("y")
-
-    # Hilfsmethode, um zu gucken, welche Farbe das aktuelle Strassenstueck haben muss
-    def which_road(self, n):
-         if (n / gl.rumbleLength) % 2 == 0:
-              return Colors.get_light()
-         else:
-              return Colors.get_dark()
 
 
     # Erstellt mit einer Hilfsklasse die einzelnen Hintergrundschichten
@@ -259,13 +145,3 @@ class Game:
     def create_player(self):
         gl.player = Player(gl.screen.get_width() / 2 - 30, gl.screen.get_height() - 100)
         gl.player_sprites.add(gl.player)
-
-    # Liest das Strassen-Array aus und entscheidet je nach Menge an Werten, ob Kurven oder Huegel zum Segment gehoeren
-    def read_road(self):
-        for x in road:
-            if len(x) == 1:
-                self.add_street(x[0])
-            elif len(x) == 2:
-                self.add_street(x[0], x[1])
-            elif len(x) == 3:
-                self.add_street(x[0], x[1], x[2])
