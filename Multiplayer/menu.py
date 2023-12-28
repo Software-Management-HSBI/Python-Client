3c5276e3d3651c77f3e5abf6d5e59161636b87ab
PDeimel <philipp.deimel@fh-bielefeld.de>
2023-12-28 10:52:39 +0100

    Hintergrundbild geändert

--- Multiplayer/menu.py
+++ Multiplayer/menu.py
@@ -10,17 +10,22 @@ import globals as gl
 
 client = Client()
 
+
 # In diesen Methoden werden unsere Aufrufe fuer die verschiedenen Modi aufgerufen, sie sind die Aktionen der Buttons
 # TODO: Das ist alles noch etwas Spaghetti-Code-Artig, irgendwie refactoren.
 def start_game():
     Game()
 
-# TODO: Bis jetzt ist nur der Singleplayer-Button nutzbar, weil wir noch keine andere Modi haben, d. h. hier dann aktualisieren
+
+# TODO: Bis jetzt ist nur der Singleplayer-Button nutzbar,
+#  weil wir noch keine andere Modi haben, d. h. hier dann aktualisieren
 def options():
     pass
 
+
 def multiplayer():
     pass
+
 
 title = "Wakaliwood Gaming"
 
@@ -31,10 +36,11 @@ quit_Button = Button(gl.width - 250, gl.height - 100, 200, 50, "Beenden", color=
 
 buttons = [start_Button, options_Button, multiplayer_Button, quit_Button]
 
-# TODO: Hier soll das Menu implementiert werden, also verschiedene Knoepfe fuer Singleplayer, Multiplayer, Einstellungen etc.
-class Menu:
 
-    background_image = pygame.image.load("assets/racer.jpg")
+# TODO: Hier soll das Menu implementiert werden,
+#  also verschiedene Knoepfe fuer Singleplayer, Multiplayer, Einstellungen etc.
+class Menu:
+    background_image = pygame.image.load("assets/backgroundMenu.png")
 
     def __init__(self):
         gl.screen = pygame.display.set_mode([gl.width, gl.height])
@@ -55,7 +61,6 @@ class Menu:
                             if button.rect.collidepoint(event.pos):
                                 if button.action:
                                     button.action()
-
 
             gl.screen.blit(self.background_image, (0, 0))
 
